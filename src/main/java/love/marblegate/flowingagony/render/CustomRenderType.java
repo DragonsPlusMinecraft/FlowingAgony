7bac5f08357bf0cce0123fe4ad721a419a32af2b
白门守望者 <marblegatekeeper@outlook.com>
2021-05-09 11:43:23 +0800

    Update Renderer and zh_cn.json
    I'm adding renderer for Cursed Hatred effect but for fxxking sake potioneffect cannot be detected on client-side!

--- src/main/java/love/marblegate/flowingagony/render/CustomRenderType.java
+++ src/main/java/love/marblegate/flowingagony/render/CustomRenderType.java
@@ -3,6 +3,7 @@ package love.marblegate.flowingagony.render;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
+import org.lwjgl.opengl.GL11;
 
 import java.util.OptionalDouble;
 
@@ -12,10 +13,11 @@ public class CustomRenderType extends RenderType {
         super(nameIn, formatIn, drawModeIn, bufferSizeIn, useDelegateIn, needsSortingIn, setupTaskIn, clearTaskIn);
     }
 
-    private static final LineState MONSTER_INDICATOR_LINE = new LineState(OptionalDouble.of(5.0D));
+    private static final LineState TARGET_INDICATOR_LINE = new LineState(OptionalDouble.of(5.0D));
 
-    public static RenderType MONSTER_ENDICATOR = makeType("flowingagony.monster_indicator", DefaultVertexFormats.POSITION_COLOR, 7, 256,
-            RenderType.State.getBuilder().line(MONSTER_INDICATOR_LINE)
+    public static RenderType PERCEIVED_MALICE_INDICATOR = makeType("flowingagony.perceived_malice_indicator",
+            DefaultVertexFormats.POSITION_COLOR, GL11.GL_QUADS, 256,
+            RenderType.State.getBuilder()
                     .transparency(TRANSLUCENT_TRANSPARENCY)
                     .texture(NO_TEXTURE)
                     .depthTest(DEPTH_ALWAYS)
@@ -23,4 +25,5 @@ public class CustomRenderType extends RenderType {
                     .lightmap(LIGHTMAP_DISABLED)
                     .writeMask(COLOR_WRITE)
                     .build(false));
+    
 }
--- src/main/java/love/marblegate/flowingagony/render/renderer/PerceivedMaliceEffectRenderer.java
+++ src/main/java/love/marblegate/flowingagony/render/renderer/PerceivedMaliceEffectRenderer.java
@@ -32,41 +32,47 @@ public class PerceivedMaliceEffectRenderer {
     }
 
     private static void highlight(MatrixStack matrixStack, IRenderTypeBuffer buffer, LivingEntity entity){
-        IVertexBuilder builder = buffer.getBuffer(CustomRenderType.MONSTER_ENDICATOR);
+        IVertexBuilder builder = buffer.getBuffer(CustomRenderType.PERCEIVED_MALICE_INDICATOR);
         Matrix4f positionMatrix = matrixStack.getLast().getMatrix();
-        pop(builder,positionMatrix, entity.world.getDayTime()%100);
+        buildShapeDynamically(builder,positionMatrix, entity.world.getDayTime()%100);
     }
 
-    private static void pop(IVertexBuilder builder, Matrix4f matrix, long standardSequence){
-        popFrame(builder,matrix,standardSequence*0.02f,0.1f,standardSequence*0.001f);
+    private static void buildShapeDynamically(IVertexBuilder builder, Matrix4f matrix, long standardSequence){
+        buildLayer(builder,matrix,standardSequence*0.02f,0.1f,standardSequence*0.001f);
         standardSequence = standardSequence-20<0? standardSequence+80: standardSequence-20;
-        popFrame(builder,matrix,standardSequence*0.02f,0.2f,standardSequence*0.001f);
+        buildLayer(builder,matrix,standardSequence*0.02f,0.2f,standardSequence*0.001f);
         standardSequence = standardSequence-20<0? standardSequence+80: standardSequence-20;
-        popFrame(builder,matrix,standardSequence*0.02f,0.3f,standardSequence*0.001f);
+        buildLayer(builder,matrix,standardSequence*0.02f,0.3f,standardSequence*0.001f);
         standardSequence = standardSequence-20<0? standardSequence+80: standardSequence-20;
-        popFrame(builder,matrix,standardSequence*0.02f,0.4f,standardSequence*0.001f);
+        buildLayer(builder,matrix,standardSequence*0.02f,0.4f,standardSequence*0.001f);
         standardSequence = standardSequence-20<0? standardSequence+80: standardSequence-20;
-        popFrame(builder,matrix,standardSequence*0.02f,0.5f,standardSequence*0.001f);
+        buildLayer(builder,matrix,standardSequence*0.02f,0.5f,standardSequence*0.001f);
     }
 
 
-    private static void popFrame(IVertexBuilder builder, Matrix4f matrix,float boxHeight, float redGrade, float alphaGrade){
-        popLine(builder, matrix, -0.5f, 0, -0.5f, 0.5f, 0, -0.5f,redGrade,alphaGrade);
-        popLine(builder, matrix, -0.5f, 0, 0.5f, 0.5f, 0, 0.5f,redGrade,alphaGrade);
+    private static void buildLayer(IVertexBuilder builder, Matrix4f matrix, float boxHeight, float redGrade, float alphaGrade){
+        add(builder, matrix, -0.5f, 0, -0.5f, redGrade,alphaGrade);
+        add(builder, matrix, 0.5f, 0, -0.5f,redGrade,alphaGrade);
+        add(builder, matrix, -0.5f, 0, 0.5f, redGrade,alphaGrade);
+        add(builder, matrix,  0.5f, 0, 0.5f,redGrade,alphaGrade);
 
-        popLine(builder, matrix, -0.5f, 0, -0.5f, -0.5f, 0, 0.5f,redGrade,alphaGrade);
-        popLine(builder, matrix, 0.5f, 0, -0.5f, 0.5f, 0, 0.5f,redGrade,alphaGrade);
+        add(builder, matrix, -0.5f, 0, -0.5f,redGrade,alphaGrade);
+        add(builder, matrix,  -0.5f, 0, 0.5f,redGrade,alphaGrade);
+        add(builder, matrix, 0.5f, 0, -0.5f,redGrade,alphaGrade);
+        add(builder, matrix,  0.5f, 0, 0.5f,redGrade,alphaGrade);
 
-        popLine(builder, matrix, -0.5f, 0, -0.5f, -0.5f, boxHeight, -0.5f,redGrade,alphaGrade);
-        popLine(builder, matrix, 0.5f, 0, -0.5f, 0.5f, boxHeight, -0.5f,redGrade,alphaGrade);
-        popLine(builder, matrix, -0.5f, 0, 0.5f, -0.5f, boxHeight, 0.5f,redGrade,alphaGrade);
-        popLine(builder, matrix, 0.5f, 0, 0.5f, 0.5f, boxHeight, 0.5f,redGrade,alphaGrade);
+        add(builder, matrix, -0.5f, 0, -0.5f,redGrade,alphaGrade);
+        add(builder, matrix, -0.5f, boxHeight, -0.5f,redGrade,alphaGrade);
+        add(builder, matrix, 0.5f, 0, -0.5f,redGrade,alphaGrade);
+        add(builder, matrix, 0.5f, boxHeight, -0.5f,redGrade,alphaGrade);
+
+        add(builder, matrix, -0.5f, 0, 0.5f,redGrade,alphaGrade);
+        add(builder, matrix, -0.5f, boxHeight, 0.5f,redGrade,alphaGrade);
+        add(builder, matrix, 0.5f, 0, 0.5f,redGrade,alphaGrade);
+        add(builder, matrix, 0.5f, boxHeight, 0.5f,redGrade,alphaGrade);
     }
-    private static void popLine(IVertexBuilder builder, Matrix4f matrix, float dx1, float dy1, float dz1, float dx2, float dy2, float dz2, float redGrade, float alphaGrade) {
-        builder.pos(matrix, dx1, dy1, dz1)
-                .color(redGrade, 0.0f, 0.0f, alphaGrade)
-                .endVertex();
-        builder.pos(matrix, dx2, dy2, dz2)
+    private static void add(IVertexBuilder builder, Matrix4f matrix, float dx, float dy, float dz, float redGrade, float alphaGrade) {
+        builder.pos(matrix, dx, dy, dz)
                 .color(redGrade, 0.0f, 0.0f, alphaGrade)
                 .endVertex();
     }
