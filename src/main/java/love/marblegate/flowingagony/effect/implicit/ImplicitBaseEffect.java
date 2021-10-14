c04aa2c05ee99af9d0bfaa47830f99c10ef5e787
白门守望者 <marblegatekeeper@outlook.com>
2021-10-15 07:59:59 +0800

    Update to 1.1.1
    Changed: Implicit Effects are now completely hidden, which is the same as in 1.16.5.

--- src/main/java/love/marblegate/flowingagony/effect/implicit/ImplicitBaseEffect.java
+++ src/main/java/love/marblegate/flowingagony/effect/implicit/ImplicitBaseEffect.java
@@ -1,11 +1,17 @@
 package love.marblegate.flowingagony.effect.implicit;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.inventory.EffectRenderingInventoryScreen;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.client.EffectRenderer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ImplicitBaseEffect extends MobEffect {
     public ImplicitBaseEffect() {
@@ -19,5 +25,34 @@ public class ImplicitBaseEffect extends MobEffect {
     @Override
     public List<ItemStack> getCurativeItems() {
         return new ArrayList<>();
+    }
+
+
+    @Override
+    public void initializeClient(Consumer<EffectRenderer> consumer) {
+        consumer.accept(new EffectRenderer() {
+            @Override
+            public boolean shouldRender(MobEffectInstance effect) {
+                return false;
+            }
+
+            @Override
+            public boolean shouldRenderInvText(MobEffectInstance effect) {
+                return false;
+            }
+
+            @Override
+            public boolean shouldRenderHUD(MobEffectInstance effect) {
+                return false;
+            }
+
+            @Override
+            public void renderInventoryEffect(MobEffectInstance effect, EffectRenderingInventoryScreen<?> gui, PoseStack mStack, int x, int y, float z) {
+            }
+
+            @Override
+            public void renderHUDEffect(MobEffectInstance effect, GuiComponent gui, PoseStack mStack, int x, int y, float z, float alpha) {
+            }
+        });
     }
 }
